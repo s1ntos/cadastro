0571c49763ac798e743fe46a98ccb5a0c6efb484
João <109487647+s1ntos@users.noreply.github.com>
2025-03-01 22:11:54 -0300

    Update DBConnection.java

--- tlcadastro/src/java/util/DBConnection.java
+++ tlcadastro/src/java/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/login_system";
+    private static final String URL = "jdbc:mysql://localhost:3306/nome-bancodedados"; //coloque o nome do seu banco de dados
     private static final String USER = "root";
-    private static final String PASSWORD = "senha";
+    private static final String PASSWORD = "senha"; //digite a senha do seu banco de dados
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
